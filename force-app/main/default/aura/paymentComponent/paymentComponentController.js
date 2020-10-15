27969bf63d24a1f110047306cbce37d464e53372
arpit-techmatrixconsulting <arpit.v@techmatrixconsulting.com>
2020-10-15 22:18:13 +0530

    Credit Memo Hand in Pay at Center button ,Downgrade and withdrawal process

--- force-app/main/default/aura/paymentComponent/paymentComponentController.js
+++ force-app/main/default/aura/paymentComponent/paymentComponentController.js
@@ -3,21 +3,61 @@
         let recordId = component.get("v.recordId");
         component.set("v.recordId", recordId);
         helper.getCurrentCredit(component, event);
-        
+        //helper.getIsOssia(component, event); // commented by nishi: 15-oct-2020: for now we get all the invoice details  on getCurrentCredit function
     }, 
     //@Juneid Filling Payment options
     handlePayNow: function(component, event, helper) {
-        component.set('v.showmodal', true);
-        var finaloptions = [];
-        //finaloptions.push({'label':'Cash','value':'Cash'});
-        finaloptions.push({'label':'Offline card','value':'Offline card'});
-        finaloptions.push({'label':'NETS','value':'NETS'});
-        component.set("v.options", finaloptions);
-        let isCheckBoxEnabled = component.get("v.isEnabled");
-        console.log('isCheckBoxEnabled-->'+isCheckBoxEnabled);
-        if(component.get("v.creditAmount") > 0){
-            console.log('in if -->');
-            component.set("v.isEnabled",false);
+        var isDueInvoice =  component.get("v.isDueInvoice");
+        console.log('isDueInvoice'+isDueInvoice);
+        if(isDueInvoice){
+            component.set('v.showmodal', true);
+            var finaloptions = [];
+            //finaloptions.push({'label':'Cash','value':'Cash'});
+            finaloptions.push({'label':'Offline card','value':'Offline card'});
+            finaloptions.push({'label':'NETS','value':'NETS'});
+            component.set("v.options", finaloptions);
+            let isCheckBoxEnabled = component.get("v.isEnabled");
+            console.log('isCheckBoxEnabled-->'+isCheckBoxEnabled);
+            if(component.get("v.creditAmount") > 0){
+                console.log('in if -->');
+                component.set("v.isEnabled",false);
+            }else{
+                component.set("v.isEnabled",true);
+            }
+        }else{
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                title : 'error',
+                message: component.get("v.errormessage"),            
+                duration:' 5000',
+                key: 'info_alt',
+                type: 'error',
+                mode: 'pester'
+            });
+            toastEvent.fire();
+            var navEvt = $A.get("e.force:navigateToSObject");
+            navEvt.setParams({
+                "recordId":component.get("v.recordId"),
+                "slideDevName": "related"
+            });
+            navEvt.fire();
+        }
+    },
+    // added by nishi: 15-oct-2020: we added this function for show paid amount details
+    handleCheckboxChange: function(component, event, helper) {
+        let isCreditAmount =component.get("v.chkValue");
+        let creditAmount = component.get("v.creditAmount");
+        let invoiceAmount =  component.get("v.invoiceAmount");
+        // we showing paid amount
+        if(isCreditAmount){
+            if(creditAmount < invoiceAmount){
+                let paidAmount = invoiceAmount - creditAmount; 
+                component.set("v.paidAmount",paidAmount);
+            }else{
+                component.set("v.paidAmount",0);
+            }
+        }else{
+            component.set("v.paidAmount", invoiceAmount);
         }
     },
     //Juneid, Method for Close 1st Model
@@ -70,8 +110,39 @@
                 console.log('state-->',state);
                 if (state === "SUCCESS") {
                     console.log('in success');
-                    var a = component.get('c.closePopup'); 
-                    $A.enqueueAction(a);
+                    component.set("v.openPopUp", false);
+                    var invoiceWrapper = response.getReturnValue();
+                    if(invoiceWrapper.errorCode == 200){
+                        var toastEvent = $A.get("e.force:showToast");
+                        toastEvent.setParams({
+                            title : 'SUCCESS',
+                            message: 'Invoice Paid Amount Successfully',            
+                            duration:' 5000',
+                            key: 'info_alt',
+                            type: 'SUCCESS',
+                            mode: 'pester'
+                        });
+                        toastEvent.fire();
+                    }else{
+                        var toastEvent = $A.get("e.force:showToast");
+                        toastEvent.setParams({
+                            title : 'error',
+                            message: invoiceWrapper.errormessage,            
+                            duration:' 5000',
+                            key: 'info_alt',
+                            type: 'error',
+                            mode: 'pester'
+                        });
+                        toastEvent.fire();
+                    }
+                   
+                    var navEvt = $A.get("e.force:navigateToSObject");
+                    navEvt.setParams({
+                        "recordId":component.get("v.recordId"),
+                        "slideDevName": "related"
+                    });
+                    navEvt.fire();
+                    
                 } else if (state === "ERROR") {
                     var errors = response.getError();
                     if (errors) {
--- force-app/main/default/aura/paymentComponent/paymentComponentHelper.js
+++ force-app/main/default/aura/paymentComponent/paymentComponentHelper.js
@@ -1,29 +1,65 @@
 ({
-	helperMethod : function() {
-		
-	},
+    helperMethod : function() {
+        
+    },
     getCurrentCredit:function (component, event) {
         let recordId = component.get("v.recordId");
-    	var getCurrentCredit = component.get("c.getCurrentCredit");
+        var getCurrentCredit = component.get("c.getCurrentCredit");
         getCurrentCredit.setParams({recordId : recordId});
         getCurrentCredit.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                console.log("From server: " + response.getReturnValue());
-                component.set("v.creditAmount", response.getReturnValue());
-             
-            } else if (state === "ERROR") {
-                    var errors = response.getError();
-                    if (errors) {
-                        if (errors[0] && errors[0].message) {
-                            console.log("Error message: " + errors[0].message);
-                        }
-                    } else {
-                        console.log("Unknown error");
+                console.log("From server: " , response.getReturnValue());
+                var invoiceWrapper = response.getReturnValue();
+                if(invoiceWrapper.errorCode == 200){
+                    //component.set("v.creditAmount", response.getReturnValue());
+                    component.set("v.creditAmount", invoiceWrapper.currentCreditAmount);
+                    component.set("v.isOssia", invoiceWrapper.isOssia);
+                    component.set("v.invoiceAmount", invoiceWrapper.totalinvoiceAmount);
+                    if(invoiceWrapper.totalinvoiceAmount > 0 ){
+                        component.set("v.paidAmount", invoiceWrapper.totalinvoiceAmount);
                     }
+                    component.set('v.isDueInvoice', true);
+                }else{
+                    component.set('v.isDueInvoice', false);
+                    component.set("v.errormessage", invoiceWrapper.errormessage);
                 }
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
         });
         $A.enqueueAction(getCurrentCredit);
-	},
+    },
+    
+    /*// commented by nishi: 15-oct-2020: for now we get all the invoice details  on getCurrentCredit function
+    getIsOssia:function (component, event) {
+        let recordId = component.get("v.recordId");
+        var getIsOssia = component.get("c.getIsOssia");
+        getIsOssia.setParams({recordId : recordId});
+        getIsOssia.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("From server: " + response.getReturnValue());
+                component.set("v.isOssia", response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(getIsOssia);
+    },*/
     
 })
