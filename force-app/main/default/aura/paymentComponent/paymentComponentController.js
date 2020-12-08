4ea57b66ed2624c4b443bb97601c1d0b8813c978
arpit-techmatrixconsulting <arpit.v@techmatrixconsulting.com>
2020-12-08 21:28:33 +0530

    Ossia Changes Part 2

--- force-app/main/default/aura/paymentComponent/paymentComponentController.js
+++ force-app/main/default/aura/paymentComponent/paymentComponentController.js
@@ -2,46 +2,13 @@
     doInit: function(component, event, helper){
         let recordId = component.get("v.recordId");
         component.set("v.recordId", recordId);
+        component.set("v.ispaidAmount", false);
         helper.getCurrentCredit(component, event);
         //helper.getIsOssia(component, event); // commented by nishi: 15-oct-2020: for now we get all the invoice details  on getCurrentCredit function
     }, 
     //@Juneid Filling Payment options
     handlePayNow: function(component, event, helper) {
-        var isDueInvoice =  component.get("v.isDueInvoice");
-        console.log('isDueInvoice'+isDueInvoice);
-        if(isDueInvoice){
-            component.set('v.showmodal', true);
-            var finaloptions = [];
-            //finaloptions.push({'label':'Cash','value':'Cash'});
-            finaloptions.push({'label':'Offline card','value':'Offline card'});
-            finaloptions.push({'label':'NETS','value':'NETS'});
-            component.set("v.options", finaloptions);
-            let isCheckBoxEnabled = component.get("v.isEnabled");
-            console.log('isCheckBoxEnabled-->'+isCheckBoxEnabled);
-            if(component.get("v.creditAmount") > 0){
-                console.log('in if -->');
-                component.set("v.isEnabled",false);
-            }else{
-                component.set("v.isEnabled",true);
-            }
-        }else{
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                title : 'error',
-                message: component.get("v.errormessage"),            
-                duration:' 5000',
-                key: 'info_alt',
-                type: 'error',
-                mode: 'pester'
-            });
-            toastEvent.fire();
-            var navEvt = $A.get("e.force:navigateToSObject");
-            navEvt.setParams({
-                "recordId":component.get("v.recordId"),
-                "slideDevName": "related"
-            });
-            navEvt.fire();
-        }
+        helper.handlePayNow(component, event);
     },
     // added by nishi: 15-oct-2020: we added this function for show paid amount details
     handleCheckboxChange: function(component, event, helper) {
@@ -86,7 +53,7 @@
             $A.enqueueAction(component.get('c.closeModel'));
             component.set("v.openPopUp", true);
         }
-        
+        component.set("v.isUseCreditMemoOnly", false);
     },
     // added condition nishi:13-nov-2020: for  if user use only credit memo amount not use other options
     handelCreditMemoSave : function (component, event) {
@@ -97,73 +64,79 @@
     //@Juneid method for genrate payment
     saveGeneratePaymet:function (component, event) {
         try {
-            let isCreditCheck = component.get("v.chkValue");
-            let recordId = component.get("v.recordId");
-            let trnsactionId = component.get("v.inputValue");
-            let currentCreditAmount = component.get("v.creditAmount");
-            let isUseCreditMemoOnly = component.get("v.isUseCreditMemoOnly");// added condition nishi:13-nov-2020: for  if user use only credit memo amount not use other options
-            console.log(isCreditCheck, recordId, trnsactionId, currentCreditAmount);
-            let param = {
-                selectedMethod : component.get('v.selectedOption'),
-                recordId : recordId, 
-                transacId : trnsactionId,
-                isCreditAmnt : isCreditCheck, 
-                currentCreditAmounts : currentCreditAmount,
-                isUseCreditMemoOnly:isUseCreditMemoOnly // added condition nishi:13-nov-2020: for  if user use only credit memo amount not use other options
-            };
-            console.log('param',param);
-            var createPayment = component.get("c.createPayment"); 
-            createPayment.setParams(param);
-            createPayment.setCallback(this, function(response) {
-                var state = response.getState();
-                console.log('state-->',state);
-                if (state === "SUCCESS") {
-                    console.log('in success');
-                    component.set("v.openPopUp", false);
-                    var invoiceWrapper = response.getReturnValue();
-                    if(invoiceWrapper.errorCode == 200){
-                        var toastEvent = $A.get("e.force:showToast");
-                        toastEvent.setParams({
-                            title : 'SUCCESS',
-                            message: 'Invoice Paid Amount Successfully',            
-                            duration:' 5000',
-                            key: 'info_alt',
-                            type: 'SUCCESS',
-                            mode: 'pester'
-                        });
-                        toastEvent.fire();
-                    }else{
-                        var toastEvent = $A.get("e.force:showToast");
-                        toastEvent.setParams({
-                            title : 'error',
-                            message: invoiceWrapper.errormessage,            
-                            duration:' 5000',
-                            key: 'info_alt',
-                            type: 'error',
-                            mode: 'pester'
-                        });
-                        toastEvent.fire();
-                    }
-                   
-                    var navEvt = $A.get("e.force:navigateToSObject");
-                    navEvt.setParams({
-                        "recordId":component.get("v.recordId"),
-                        "slideDevName": "related"
-                    });
-                    navEvt.fire();
-                    
-                } else if (state === "ERROR") {
-                    var errors = response.getError();
-                    if (errors) {
-                        if (errors[0] && errors[0].message) {
-                            console.log("Error message: " + errors[0].message);
+            let ispaidAmount = component.get("v.ispaidAmount");
+            if(!ispaidAmount){
+                component.set("v.ispaidAmount", true);
+                let isCreditCheck = component.get("v.chkValue");
+                let recordId = component.get("v.recordId");
+                let trnsactionId = component.get("v.inputValue");
+                let currentCreditAmount = component.get("v.creditAmount");
+                let isUseCreditMemoOnly = component.get("v.isUseCreditMemoOnly");// added condition nishi:13-nov-2020: for  if user use only credit memo amount not use other options
+                let selectedMethod = component.get("v.selectedOption");
+                console.log(isCreditCheck, recordId, trnsactionId, currentCreditAmount,selectedMethod,isUseCreditMemoOnly);
+                let param = {
+                    selectedMethod : selectedMethod,
+                    recordId : recordId, 
+                    transacId : trnsactionId,
+                    isCreditAmnt : isCreditCheck, 
+                    currentCreditAmounts : currentCreditAmount,
+                    isUseCreditMemoOnly:isUseCreditMemoOnly // added condition nishi:13-nov-2020: for  if user use only credit memo amount not use other options
+                };
+                console.log('param',param);
+                var createPayment = component.get("c.createPayment"); 
+                createPayment.setParams(param);
+                createPayment.setCallback(this, function(response) {
+                    var state = response.getState();
+                    console.log('state-->',state);
+                    if (state === "SUCCESS") {
+                        console.log('in success');
+                        component.set("v.openPopUp", false);
+                        var invoiceWrapper = response.getReturnValue();
+                        if(invoiceWrapper.errorCode == 200){
+                            var toastEvent = $A.get("e.force:showToast");
+                            toastEvent.setParams({
+                                title : 'SUCCESS',
+                                message: 'Invoice Paid Amount Successfully',            
+                                duration:' 5000',
+                                key: 'info_alt',
+                                type: 'SUCCESS',
+                                mode: 'pester'
+                            });
+                            toastEvent.fire();
+                            location.reload();
+                        }else{
+                            var toastEvent = $A.get("e.force:showToast");
+                            toastEvent.setParams({
+                                title : 'error',
+                                message: invoiceWrapper.errormessage,            
+                                duration:' 5000',
+                                key: 'info_alt',
+                                type: 'error',
+                                mode: 'pester'
+                            });
+                            toastEvent.fire();
                         }
-                    } else {
-                        console.log("Unknown error");
+                    
+                        var navEvt = $A.get("e.force:navigateToSObject");
+                        navEvt.setParams({
+                            "recordId":component.get("v.recordId"),
+                            "slideDevName": "related"
+                        });
+                        navEvt.fire();
+                        
+                    } else if (state === "ERROR") {
+                        var errors = response.getError();
+                        if (errors) {
+                            if (errors[0] && errors[0].message) {
+                                console.log("Error message: " + errors[0].message);
+                            }
+                        } else {
+                            console.log("Unknown error");
+                        }
                     }
-                }
-            });
-            $A.enqueueAction(createPayment);
+                });
+                $A.enqueueAction(createPayment);
+            }
         } catch(err) {
             console.log("err", err);
         }
